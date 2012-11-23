91463228af713970fab2f78e3b4d37099abc51b4
Tugdual Saunier <tugdual.saunier@gmail.com>
2012-11-23 10:06:18 +0100

    [Sismo] added a generic Sismo configuration

---
+++ .sismo/SismoConfigGenerator.php
@@ -0,0 +1,107 @@
+<?php
+
+class SismoConfigGenerator
+{
+    const TEMPLATE = <<<EOF
+%array_key% => (new Sismo\Project(%name%))
+    ->setRepository(%path%)
+    ->setSlug(%slug%)
+    ->setCommand(%command%)
+    ->addNotifier(\$notifier),
+
+EOF
+    ;
+
+    static $poolCommands = array(
+        'vendors'   => array(
+            'deps'          => 'php bin/vendors install',
+            'composer.json' => 'composer install --dev',
+        ),
+        'tests'     => array(
+            'Makefile'           => 'make test',
+            'tests.sh'           => 'sh tests.sh',
+            'run-tests.sh'       => 'sh run-tests.sh',
+            'app'                => 'phpunit -c app/',
+            'phpunit.xml.dist'   => 'phpunit',
+            'phpunit.xml'        => 'phpunit',
+            'lib/vendor/symfony' => 'php symfony test:all',
+        ),
+    );
+
+    protected $paths = array();
+
+    public function addPath(\SplFileInfo $directory)
+    {
+        $this->paths[] = $directory;
+
+        return $this;
+    }
+
+    public function dump()
+    {
+        $configs = array();
+
+        foreach ($this->paths as $index => $path) {
+            $configs[] = $this->generateProjectConfig($path, $index);
+        }
+
+        return sprintf('<?php
+
+return \SplFixedArray::fromArray(array(
+%s
+));
+', implode("\n", $configs));
+    }
+
+    public function escape($value)
+    {
+        return var_export($value, true);
+    }
+
+    protected function generateProjectConfig(\SplFileInfo $directory, $index = 0)
+    {
+        $values = $this->getProjectValues($directory, $index);
+
+        return str_replace(array_keys($values), array_map(array($this, 'escape'), array_values($values)), self::TEMPLATE);
+    }
+
+    protected function getProjectCommand($path)
+    {
+        $commands = array();
+        foreach (self::$poolCommands as $pool) {
+            foreach ($pool as $file => $command) {
+                if(file_exists($path.'/'.$file)) {
+                    $commands[] = $command;
+                    break;
+                }
+            }
+        }
+
+        return trim(implode(";\n", $commands));
+    }
+
+    protected function getProjectValues(\SplFileInfo $directory, $index = 0)
+    {
+        $path = $directory->getPathname();
+        $slug = $directory->getFilename();
+
+        $descriptionFile = sprintf('%s/.git/description', $path);
+        $description     = false;
+
+        if (file_exists($descriptionFile)) {
+            $description = trim(file_get_contents($directory->getPathname().'/.git/description'));
+        }
+
+        if (!$description || FALSE !== strpos($description, 'Unnamed repository')) {
+            $description = $slug;
+        }
+
+        return array(
+            '%array_key%' => $index,
+            '%name%'      => $description,
+            '%path%'      => $path,
+            '%slug%'      => $slug,
+            '%command%'   => $this->getProjectCommand($path),
+        );
+    }
+}
---
+++ .sismo/config.php
@@ -0,0 +1,33 @@
+<?php
+
+// $app['debug'] = true;
+
+define('CACHE_FILE', $app['data.path'].'/cache/config.cache.php');
+define('CACHE_LIFE', 7200); // 2h
+
+if ( (time() - @filemtime(CACHE_FILE)) > CACHE_LIFE || $app['debug']) {
+    require __DIR__.'/SismoConfigGenerator.php';
+
+    $generator = new SismoConfigGenerator();
+    $finder = Symfony\Component\Finder\Finder::create()
+        ->directories()
+        ->depth('< 1')
+        ->in(getenv('HOME').'/Work')
+        ->notName('*.bak')
+        ->filter(function($directory){
+            return is_dir($directory->getPathname().'/.git');
+        })
+    ;
+
+    clearstatcache();
+    foreach ($finder as $directory) {
+        $generator->addPath($directory);
+    }
+
+    file_put_contents(CACHE_FILE, $generator->dump());
+}
+
+$mailNotifier = new Sismo\Notifier\MailNotifier('tucksaun@gmail.com', '%status%', '%status%');
+$notifier = new Sismo\Contrib\CrossFingerNotifier(array($mailNotifier));
+
+return require CACHE_FILE;
