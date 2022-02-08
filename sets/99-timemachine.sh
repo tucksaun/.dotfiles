e25b2f998709da6f8c5b30bf1c45eed92699dd20
Tugdual Saunier <tugdual.saunier@gmail.com>
2022-02-07 19:34:33 -0500

    [TimeMachine] use fixed path exclusions

--- sets/99-timemachine.sh
+++ sets/99-timemachine.sh
@@ -3,7 +3,7 @@ set -e
 
 echo " ** Setuping TimeMachine **"
 for exclusion in $HOME/Pictures/Raw "$HOME/Pictures/Lightroom Catalog" $HOME/Music $HOME/Movies $HOME/Library/Caches "$HOME/Library/Application Support/JetBrains" $HOME/.composer /Applications/Xcode.app "/Applications/Microsoft Word.app" "/Applications/Microsoft Excel.app" "/Applications/Microsoft PowerPoint.app" "/Applications/Adobe Creative Cloud" "/Applications/Adobe Photoshop 2022" "/Applications/Adobe Lightroom Classic"; do
-    sudo tmutil addexclusion "$exclusion"
+    sudo tmutil addexclusion -p "$exclusion"
 done
 
 if ! sudo tmutil destinationinfo | grep -i Phoenyx; then
