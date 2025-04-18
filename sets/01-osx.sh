17788ee221fbbecec0619d771e0bd64726e4e0a6
Tugdual Saunier <tugdual.saunier@gmail.com>
2025-04-18 17:23:14 +0200

    Remove deprecated defaults settings

--- sets/01-osx.sh
+++ sets/01-osx.sh
@@ -33,12 +33,6 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 # Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Use scroll gesture with the Ctrl (^) modifier key to zoom
-defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
-defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
-# Follow the keyboard focus while zoomed in
-defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
-
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
 
