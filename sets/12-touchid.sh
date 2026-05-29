e1d49776736d4cb62fec622e22aa450bfe3f4b97
Tugdual Saunier <tugdual.saunier@gmail.com>
2026-05-29 15:28:28 +0200

    -

--- sets/12-touchid.sh
+++ sets/12-touchid.sh
@@ -8,13 +8,13 @@ auth       sufficient     pam_tid.so\
 ' /etc/pam.d/sudo
 fi
 
-# If the directory is not present it means the OS will not support LaunchAgents
-# and we should not try to copy the plist file or load it
-if [ -d  ~/Library/LaunchAgents ]; then
-    cp "${DOTFILE_DIR}/me.tucksaun.ssh-agent.plist" ~/Library/LaunchAgents/me.tucksaun.ssh-agent.plist
-    launchctl load -w ~/Library/LaunchAgents/me.tucksaun.ssh-agent.plist
-    launchctl stop "user/$UID/com.openssh.ssh-agent"
-    launchctl disable "user/$UID/com.openssh.ssh-agent"
-    launchctl enable "user/$UID/me.tucksaun.ssh-agent"
-    launchctl start "user/$UID/me.tucksaun.ssh-agent"
-fi
+# # If the directory is not present it means the OS will not support LaunchAgents
+# # and we should not try to copy the plist file or load it
+# if [ -d  ~/Library/LaunchAgents ]; then
+#     cp "${DOTFILE_DIR}/me.tucksaun.ssh-agent.plist" ~/Library/LaunchAgents/me.tucksaun.ssh-agent.plist
+#     launchctl load -w ~/Library/LaunchAgents/me.tucksaun.ssh-agent.plist
+#     launchctl stop "user/$UID/com.openssh.ssh-agent"
+#     launchctl disable "user/$UID/com.openssh.ssh-agent"
+#     launchctl enable "user/$UID/me.tucksaun.ssh-agent"
+#     launchctl start "user/$UID/me.tucksaun.ssh-agent"
+# fi
